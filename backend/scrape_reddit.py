fe40679bebcb0d3f39a4812dc99e6cfcf6d7076c
SamLee514 <samuel.r.lee@vanderbilt.edu>
2020-09-20 01:10:27 -0400

    my milkshakes bring all the json documents to the ibm cloudant db

--- backend/scrape_reddit.py
+++ backend/scrape_reddit.py
@@ -1,10 +1,19 @@
 # Reference: https://praw.readthedocs.io/en/latest/
 # !pip install praw
 
-from pymongo import MongoClient
+# from pymongo import MongoClient
+from cloudant.client import Cloudant
+from cloudant.error import CloudantException
+from cloudant.result import Result, ResultByKey
 import praw
 from praw.models import MoreComments
 
+# IBM Cloudant TODO: don't display API key
+client = Cloudant.iam('91a4a244-2758-4011-9e62-c11817074ca5-bluemix', '5QulfkQHwctHPNKddeeGeqA8tnHm-oAhCTI-M_pWvtu4')
+client.connect()
+
+db = client['text']
+print(db)
 
 # Get 10 new posts from the COVID subreddit
 # r/COVID
@@ -34,6 +43,21 @@ for post in new_posts:
             print("---", comment_replies.body)
     post_num += 1
     print("\n \n \n")
+
+    # Load into db
+    json_document = {
+        'postNumber': post_num,
+        'postID': post.id,
+        'postTitle': post.title,
+        'postContent': post.selftext,
+        # 'comments': TODO: idk how much to integrate comments rn
+    }
+
+    # Create new document
+    new_document = db.create_document(json_document)
+    # Check that the document exists in the db
+    if new_document.exists():
+        print(f"Document '{post_num}' successfully created")
 
 
 # Get subreddit data
